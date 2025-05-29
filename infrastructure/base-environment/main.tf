dd552fdef7dad3b44a13ee29ab434265d7c52eb6
Owain Kenway <o.kenway@ucl.ac.uk>
2025-05-29 13:55:24 +0100

    Try to fix variable dependence

--- infrastructure/base-environment/main.tf
+++ infrastructure/base-environment/main.tf
@@ -1,3 +1,8 @@
+locals {
+  namespace = "{var.username}-comp0235-ns"
+  network_name = "{var.username}-comp0235-ns/ds4eng"
+}
+
 data "harvester_image" "img" {
   display_name = var.img_display_name
   namespace    = "harvester-public"
@@ -5,7 +10,7 @@ data "harvester_image" "img" {
 
 data "harvester_ssh_key" "mysshkey" {
   name      = var.keyname
-  namespace = var.namespace
+  namespace = locals.namespace
 }
 
 resource "random_id" "secret" {
@@ -14,7 +19,7 @@ resource "random_id" "secret" {
 
 resource "harvester_cloudinit_secret" "cloud-config" {
   name      = "cloud-config-${random_id.secret.hex}"
-  namespace = var.namespace
+  namespace = locals.namespace
 
   user_data = templatefile("cloud-init.tmpl.yml", {
       public_key_openssh = data.harvester_ssh_key.mysshkey.public_key
@@ -26,7 +31,7 @@ resource "harvester_virtualmachine" "vm" {
   count = var.vm_count
 
   name                 = "${var.username}-base-${format("%02d", count.index + 1)}-${random_id.secret.hex}"
-  namespace            = var.namespace
+  namespace            = locals.namespace
   restart_after_update = true
 
   description = "Base VM"
@@ -46,7 +51,7 @@ resource "harvester_virtualmachine" "vm" {
     name           = "nic-1"
     wait_for_lease = true
     type           = "bridge"
-    network_name   = var.network_name
+    network_name   = locals.network_name
   }
 
   disk {
