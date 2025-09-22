0463ca2ac5a1495714288b18c2092d78cf0dc724
Owain Kenway <o.kenway@ucl.ac.uk>
2025-09-22 13:49:15 +0100

    Import comp0235 coursework

---
+++ coursework/comp0235/lecture3a_dataengexample/key_management/terraform/varibles.tf
@@ -0,0 +1,15 @@
+variable namespace {
+  type = string
+#  default = "<username>-comp0235-ns"
+}
+
+variable keyname {
+  type = string
+#  default = "<username>-cnc"
+}
+
+variable keyfile {
+  type = string
+#  default = "/home/almalinux/.ssh/id_ed25519.pub"
+}
+
