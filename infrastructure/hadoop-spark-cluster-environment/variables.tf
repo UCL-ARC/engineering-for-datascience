a834c5648f33786f140a726626aace7eda5675f1
Owain Kenway <o.kenway@ucl.ac.uk>
2025-09-23 14:02:17 +0100

    Base environment files

---
+++ infrastructure/hadoop-spark-cluster-environment/variables.tf
@@ -0,0 +1,19 @@
+variable img_display_name {
+  type = string
+  default = "almalinux-9-genericcloud-9.5-20241120"
+}
+
+variable username {
+  type = string
+#  default = ""
+}
+
+variable keyname {
+  type = string
+#  default = ""
+}
+
+variable vm_count {
+  type    = number
+  default = 4
+}
